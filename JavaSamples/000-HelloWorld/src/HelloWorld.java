9ea7eba5aee570c721f027cf4a3de4222cdb8912
utin0l <utin0l@debian>
2021-01-18 21:47:19 +0100

    First version

---
+++ JavaSamples/000-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+// Ceci est une classe
+public class HelloWorld {
+
+	// Ceci est une méthode statique
+	public static void main(String[] args) {
+		// out est une PrintStream
+		// out est un attribut statique de la classe System
+		// Une classe commence par une majuscule
+		// Un attribut commence par une minuscule
+		System.out.println("Hello World !!!");
+		System.out.println("Second line"); //ln est un saut de ligne
+		System.out.print("line");
+		System.out.print("line");
+		System.out.print("line");
+		System.out.print("\n line");
+		System.out.print("\t line");	
+	}
+}
