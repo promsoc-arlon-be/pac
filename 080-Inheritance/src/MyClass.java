efed39414a53342a56bd288f5f489083b89e51b7
utin0l <utin0l@debian>
2021-01-18 21:38:04 +0100

    First version

---
+++ 080-Inheritance/src/MyClass.java
@@ -0,0 +1,14 @@
+
+public class MyClass {
+
+	// digression
+	public String printIt(int i) {
+		System.out.println("Le nom de la Classe est: ");
+		return ("MyClass");
+	}
+	// fin digression
+
+	public void printIt() {
+		System.out.println("Le nom de la Classe est: ");
+	}
+}
