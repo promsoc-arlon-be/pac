23700e428bcc078cf8a1e74e5a1bde4cc257f460
utin0l <utin0l@debian>
2021-01-26 13:21:02 +0100

    désolée, je suis encore au mauvais endroit

---
+++ 090-Association/src/A.java
@@ -0,0 +1,22 @@
+// A ---> B
+// 1 ---> 1
+
+public class A 
+{
+
+	private B b;
+
+	public B getB() 
+	{
+		return (b);
+	}
+
+	public void setB(B b) 
+	{
+		this.b = b;
+	}
+	public void print()
+	{
+		System.out.println("Je suis un A");
+	}
+}
