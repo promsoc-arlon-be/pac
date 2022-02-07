ed9d0d50b43cc40d8519ca9dc40dccf51a36462c
Alan192 <graymeylan@gmail.com>
2022-02-07 22:13:42 +0100

    j'avais oublié de stage

--- OOClock/src/Clock.java
+++ OOClock/src/Clock.java
@@ -1,4 +1,48 @@
 
 public class Clock {
-//test
-}
+
+	boolean displayFormat;
+	Time time = new Time();
+
+	public Clock() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void setTime(int hours, int minutes, int seconds) {
+		this.time.hours = hours;
+		this.time.minutes = minutes;
+		this.time.seconds = seconds;
+	}
+	
+	public void nextSecond() {
+		if(this.time.seconds < 59) {
+			this.time.seconds += this.time.seconds;
+		}
+		else if(this.time.seconds == 59) {
+			
+			this.time.seconds = 0;
+			
+			if(this.time.minutes < 59) {
+				this.time.minutes += this.time.minutes;
+			}
+			else if(this.time.minutes == 59) {
+				
+				this.time.minutes = 0;
+				
+				if(this.time.hours < 23) {
+					this.time.hours += this.time.hours;
+				}
+				else if(this.time.hours == 24) {
+					this.time.hours = 1;
+				}
+			}
+		}
+	}
