9adc43bf54c4fa38f17074ceb8a4f1238f4fabc3
pva <philippe.vanderheyden@promsoc-arlon.be>
2022-02-07 21:15:54 +0100

    Implementation code added.

---
+++ OOClock/src/Clock.java
@@ -0,0 +1,9 @@
+
+public class Clock {
+
+	public void nextSecond() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- OOClock/src/SecondsTimer.java
+++ OOClock/src/SecondsTimer.java
@@ -1,7 +1,11 @@
 
 public class SecondsTimer {
+	Clock clock;
 
-	public void startTicking() {
+	public void startTicking() throws InterruptedException {
+			Thread.sleep(1000);
+			clock.nextSecond();
+		
 	}
 
 	public void stopTicking() {
