a72934751798dfcbb92ccb364e95ce8a2f0a9411
Alan192 <graymeylan@gmail.com>
2022-02-14 21:26:44 +0100

    Merge branch 'main' of https://github.com/promsoc-arlon-be/pac.git into main

--- OOClock/src/Test.java
+++ OOClock/src/Test.java
@@ -4,13 +4,14 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Clock cl = new Clock();
+		cl.setTime(19, 58, 53);
 		
 		SecondsTimer st = new SecondsTimer(cl);
 		try {
 			st.startTicking();
 			Thread.sleep(10*1000);
 		}catch(InterruptedException e) {
-			System.out.println("J'ai été interrompu ...")
+			System.out.println("J'ai été interrompu ...");
 		}
 		st.stopTicking();
 	}
