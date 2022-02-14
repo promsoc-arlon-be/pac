4a9bb314c7cb8c1f66f065a9decb1e6906dea069
Alan192 <alanmeyer192@gmail.com>
2022-02-14 20:05:52 +0100

    clear didn't work

--- OOClock/src/Clock.java
+++ OOClock/src/Clock.java
@@ -37,11 +37,6 @@ public class Clock {
 	}
 	
 	
-	public static void clearScreen() {  
-	    System.out.print("\033[H\033[2J");  
-	    System.out.flush();  
-	}  
-	
 	public void setTime(int hours, int minutes, int seconds) {
 		this.time.hours = hours;
 		this.time.minutes = minutes;
@@ -71,7 +66,6 @@ public class Clock {
 				}
 			}
 		}
-		clearScreen();
 		displayFormat(24);
 	}
 	
