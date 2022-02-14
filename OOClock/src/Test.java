5c4bbf3d420a43414d9d6b5998619d6044625bc9
Alan192 <alanmeyer192@gmail.com>
2022-02-14 21:20:43 +0100

    good test

--- OOClock/src/Test.java
+++ OOClock/src/Test.java
@@ -3,9 +3,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Clock c = new Clock();
-		c.setTime(19, 54, 58);
-		c.displayFormat(24);
+		Clock cl = new Clock();
+		
+		SecondsTimer st = new SecondsTimer(cl);
+		try {
+			st.startTicking();
+			Thread.sleep(10*1000);
+		}catch(InterruptedException e) {
+			System.out.println("J'ai été interrompu ...")
+		}
+		st.stopTicking();
 	}
 
 }
