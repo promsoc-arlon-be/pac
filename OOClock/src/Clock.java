edac30f6582f8fb432cbc5e474cb5bab3d286af1
Alan192 <graymeylan@gmail.com>
2022-02-14 19:28:59 +0100

    before delete

--- OOClock/src/Clock.java
+++ OOClock/src/Clock.java
@@ -3,22 +3,44 @@ public class Clock {
 
 	boolean displayFormat;
 	Time time;
-
+	boolean stopped = false;
+	int displayedHour = 0;
+	
 	public Clock() {
 		this.time = new time();
 		this.time.hours = this.time.minutes = this.time.seconds = 0;
 	}
 	
 	public void start() {
-		
+		stopped = false;
+		while(!stopped) {
+			Thread.sleep(1000);
+			nextSecond();
+		}
 	}
 	
 	public void stop() {
-		
+		stopped = true;
 	}
-	public void showTime() {
-		System.outprintln("Il est actuellement " + this.time.hours + ":" + this.time.minutes + ":" + this.time.seconds);
+	
+	public void displayFormat(int value) {
+		displayedHour = this.time.hours;
+		if(value == 12) {
+			if(displayedHour == 24) {
+				displayedHour = 0;
+			}
+			else if(displayedHour > 12){
+				displayedHour = displayedHour - 12;
+			}
+		}		
+		System.outprintln("Il est actuellement " + displayedHour + ":" + this.time.minutes + ":" + this.time.seconds);
 	}
+	
+	
+	public static void clearScreen() {  
+	    System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	}  
 	
 	public void setTime(int hours, int minutes, int seconds) {
 		this.time.hours = hours;
@@ -49,4 +71,6 @@ public class Clock {
 				}
 			}
 		}
+		clearScreen();
+		displayFormat(24);
 	}
--- OOClock/src/Test.java
+++ OOClock/src/Test.java
@@ -3,7 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
