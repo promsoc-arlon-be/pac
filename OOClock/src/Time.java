f52bacb02445d53ada29f0a4a4caa084fba873b6
Jerome3006 <jerome.ensch@hotmail.com>
2022-02-14 19:22:05 +0100

    Update Time class

--- OOClock/src/Time.java
+++ OOClock/src/Time.java
@@ -5,5 +5,43 @@ public class Time {
 	int minutes;
 	int seconds;
 	
+	public void nextSecond()
+	{
+		if(seconds < 60)
+		{
+			seconds += 1;
+		}
+		else 
+		{
+			seconds = 0;
+			nextMinutes();
+		}
+	}
 	
+	public void nextMinutes()
+	{
+		if(minutes < 60)
+		{
+			minutes += 1;
+		}
+		else 
+		{
+			minutes = 0;
+			nextHours();
+		}
+	}
+	
+	public void nextHours()
+	{
+		if(hours < 24)
+		{
+			hours += 1;
+		}
+		else
+		{
+			hours = 0;
+			nextSecond();
+		}
+		
+	}
 }
