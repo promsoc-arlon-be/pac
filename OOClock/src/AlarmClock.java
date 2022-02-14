1f16116c23de14dec48b0dc49f8c6d0955d529d7
Valentin <v_dimitrov@ymail.com>
2022-02-14 19:12:08 +0100

    1e traduction PPT/Squeak vers Java

--- OOClock/src/AlarmClock.java
+++ OOClock/src/AlarmClock.java
@@ -1,9 +1,77 @@
 
-public class AlarmClock /* extends Clock */ {
-	//alarm
+public class AlarmClock extends Clock {
+	/*
+	 * OOP for AlarmClock Language dependent: How represent the AlarmThingToDo?
+	 * 
+	 * In Squeak: Use a block
+	 * 
+	 * Clock subclass: #AlarmClock
+	 * 
+	 * instanceVariableNames: 'alarmTime alarmBlock '
+	 * 
+	 * classVariableNames: ''
+	 * 
+	 * poolDictionaries: ''
+	 * 
+	 * category:
+	 * 
+	 * alarmBlock: aBlock
+	 * 
+	 * 	alarmBlock := aBlock.
+	 * 
+	 * alarmTime
+	 * 
+	 * 	^alarmTime
+	 * 
+	 * setAlarmTime: aString
+	 * 
+	 * 	alarmTime := Time readFrom:
+	 * 
+	 * 		(ReadStream on: aString).
+	 * 
+	 * nextSecond delegates up to Clock, then checks the alarmTime 
+	 * 
+	 * nextSecond 
+	 * 
+	 * 	super nextSecond. 
+	 * 
+	 * 	(time = alarmTime) 
+	 * 
+	 * 		ifTrue: [alarmBlock value].
+	 * 
+	 * cl := AlarmClock new. 
+	 * 
+	 * cl setTime: '2:04 pm'. 
+	 * 
+	 * cl alarmBlock: 
+	 * 
+	 * 	[3 timesRepeat:
+	 * 
+	 * 		[Smalltalk beep. Transcript show: 'ALARM!']]. 
+	 * 
+	 * cl setAlarmTime: '2:05 pm'. 
+	 * 
+	 * cl start "Don't forget cl stop later!"
+	 */
 	
-	//Time alarmTime = new Time();
+	// alarm ???
+
+	Time alarmTime = new Time();
 	
-	//nextSecond()
+	public Time getAlarmTime() {
+		return alarmTime;
+	}
+
+	public void setAlarmTime(Time alarmTime) {
+		this.alarmTime = alarmTime;
+		// conseillé String ???
+	}
 	
+	 public void nextSecond() {
+	  
+	  	// super.nextSecond; 
+	 
+	  	// ifTrue(Time = alarmTime){ }
+	 }
+
 }
