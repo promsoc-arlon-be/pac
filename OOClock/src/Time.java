f2ba40157dd6a28bd61ed75ac1cce2897f6e221b
Jerome3006 <jerome.ensch@hotmail.com>
2022-02-14 20:12:30 +0100

    Merge 2

--- OOClock/src/Time.java
+++ OOClock/src/Time.java
@@ -7,33 +7,33 @@ public class Time {
 	
 	public void nextSecond()
 	{
-		if(seconds < 60)
+		if(seconds < 59)
 		{
 			seconds += 1;
 		}
 		else 
 		{
 			seconds = 0;
-			nextMinutes();
+			nextMinute();
 		}
 	}
 	
-	public void nextMinutes()
+	public void nextMinute()
 	{
-		if(minutes < 60)
+		if(minutes < 59)
 		{
 			minutes += 1;
 		}
 		else 
 		{
 			minutes = 0;
-			nextHours();
+			nextHour();
 		}
 	}
 	
-	public void nextHours()
+	public void nextHour()
 	{
-		if(hours < 24)
+		if(hours < 23)
 		{
 			hours += 1;
 		}
@@ -42,6 +42,5 @@ public class Time {
 			hours = 0;
 			nextSecond();
 		}
-		
 	}
 }
