21e18ea421884f610189bfda04ca590ba1673247
pva <philippe.vanderheyden@promsoc-arlon.be>
2023-03-06 21:49:12 +0100

    test removal

--- Clock2023Last/src/Test.java
+++ Clock2023Last/src/Test.java
@@ -2,8 +2,6 @@ import java.util.Date;
 
 public class Test {
 
-
-
 	public static void main(String[] args) {
 		Clock c = new Clock();
 		SecondTimer st = new SecondTimer(c);
@@ -15,21 +13,24 @@ public class Test {
 			e.printStackTrace();
 		}
 		st.stopTicking();
-		
-		 long start = new Date().getTime();
-		 long stop  = new Date().getTime();
-		 System.out.println(start);
-		 while (stop - start < 1000)
-		 {
-			 stop  = new Date().getTime();
-			 System.out.println(stop);
-		 } 
-		 System.out.println("End");
-		 
-		 Time t = new Time();
-		 for(int i = 0; i < 3 * 60; i++)
-		 {t.nextSecond(); System.out.println(t);}
-		 
+
+		// busy
+		if (0 == 1) {
+
+			long start = new Date().getTime();
+			long stop = new Date().getTime();
+			System.out.println(start);
+			while (stop - start < 1000) {
+				stop = new Date().getTime();
+				System.out.println(stop);
+			}
+			System.out.println("End");
+		}
+		/*
+		 * Time t = new Time(); for(int i = 0; i < 3 * 60; i++) {t.nextSecond();
+		 * System.out.println(t);}
+		 */
+
 	}
-	
+
 }
