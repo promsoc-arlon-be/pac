8741788ff4d4aec114dc780a647dc0d036a99e75
pva <philippe.vanderheyden@promsoc-arlon.be>
2023-03-06 19:47:52 +0100

    Fix package of Time

---
+++ Clock23/src/mypkg/Test.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Time t = new Time();
+		System.out.println(t);
+
+	}
+
+}
---
+++ Clock23/src/mypkg/Time.java
@@ -0,0 +1,10 @@
+package mypkg;
+
+public class Time {
+
+	@Override
+	public String toString() {
+		return "Hello Time []";
+	}
+
+}
