0a823109ab86fc4756a28ca59f0b196a91cc2f24
pva <philippe.vanderheyden@promsoc-arlon.be>
2023-03-06 21:45:36 +0100

    Clock update

--- Clock2023Last/src/Clock.java
+++ Clock2023Last/src/Clock.java
@@ -1,69 +1,67 @@
 public class Clock {
 
-		
-		/*----- Variable ----*/
-		
-		boolean displayFormat;
-		Time time;
-		boolean stop = false;
-		int Hour = 0;
-		
-		/*---- Constructor ----*/
-		
-		public Clock(){
-			this.time = new Time();
-			this.time.hour = this.time.minute = this.time.second = 0;
+	/*----- Variable ----*/
+
+	public final static int H24 = 24;
+	public final static int H12 = 12;
+	boolean displayFormat;
+	Time time;
+	boolean stop = false;
+	int diplayHour = 0;
+
+	/*---- Constructor ----*/
+
+	public Clock() {
+		this.time = new Time();
+		this.time.hour = this.time.minute = this.time.second = 0;
+	}
+
+	/*----- Procedure start -----
+	 * setting boolean stop as false
+	 * working until stop became true
+	 * */
+	public void start() {
+		stop = false;
+		while (!stop) {
+			nextSecond();
 		}
-		
-		/*----- Procedure start -----
-		 * setting boolean stop as false
-		 * working until stop became true
-		 * */
-		public void start() {
-			stop = false;
-			while (!stop) {
-				nextSecond();
+	}
+
+	/*----- Procedure stop -----
+	 * setting boolean stop as true
+	 * */
+	public void stop() {
+		stop = true;
+	}
+
+	/*----- Procedure affichage heure -----
+	 * @param value integer
+	 * */
+	public void displayFormat(int value) {
+		diplayHour = this.time.hour;
+		if (value == 12) {
+			if (diplayHour == 24) {
+				diplayHour = 0;
+			} else if (diplayHour > 12) {
+				diplayHour = diplayHour - 12;
 			}
 		}
-		
-		/*----- Procedure stop -----
-		 * setting boolean stop as true
-		 * */
-		public void stop() {
-			stop = true;
-		}
-		
-		/*----- Procedure affichage heure -----
-		 * @param value integer
-		 * */
-		public void displayFormat(int value) {
-			Hour = this.time.hour;
-			if (value == 12) {
-				if (Hour == 24) {
-					Hour = 0;
-				} else if (Hour > 12) {
-					Hour = Hour - 12;
-				}
-			}
-			System.out.println("Actual time is:  " + Hour + ":" + this.time.minute + ":" + this.time.second);
-		}
-		
-		/*----- Procedure setting time ------*/
-		public void setTime(int hours, int minutes, int seconds) {
-			this.time.hour = hour;
-			this.time.minute = minute;
-			this.time.second = second;
-		}
-		
-		/*----- Procedure changement de seconde*/
-		public void nextSecond() {
-			/*La classe time doit avoir une procedure next fonction*/
-			time.nextSecond();
-			/* Affichage en mode 24h */
-			displayFormat(24);
-		}
+		System.out.println("Actual time is:  " + diplayHour + ":" + this.time.minute + ":" + this.time.second);
+	}
 
+	/*----- Procedure setting time ------*/
+	public void setTime(int hours, int minutes, int seconds) {
+		this.time.hour = hours;
+		this.time.minute = minutes;
+		this.time.second = seconds;
+	}
 
+	/*----- Procedure changement de seconde*/
+	public void nextSecond() {
+		/* La classe time doit avoir une procedure next fonction */
+		time.nextSecond();
+		/* Affichage en mode 24h */
+		displayFormat(H24);
 	}
 
 }
